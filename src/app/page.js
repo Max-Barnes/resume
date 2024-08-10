f2efecffe7a49664be8769b14aa7050bd11d6b55
Max Barnes <maxbarnes.dev@gmail.com>
2024-08-10 15:59:24 -0400

    time to write write write about meee

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,53 @@
-import Link from "next/link";
-
 export default function Landing() {
   return (
     <main>
       <div class="navbar">
-        <div>
-          <Link href="/about">About</Link>
+        <p class="logo">Max Barnes</p>
+        <ul class="nav-items">
+          <li>About</li>
+          <li>Education</li>
+          <li>Experience</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
+      <div class="container">
+        <div class="section-about">
+          <h1>About</h1>
+          <p>
+            I'm Max Barnes, and I am a creator at heart. Whether it's
+            conceptualizing, creative writing, or figuring out how to make
+            day-to-day life easier; I am always driven by the idea of what could
+            be. I've cultivated leadership experience and customer service
+            skills throughout my career across various industries. However, my
+            journey into creating video games as a hobby sparked my fascination
+            with coding and its potential to take my ideas and build them into
+            anything.{" "}
+          </p>
         </div>
         <div>
-          <Link href="/portfolio">Portfolio</Link>
+          <h1>Education</h1>
+          <p>
+            I transitioned to the tech field because I recognized the growing
+            demand for the strong interpersonal soft skills I had developed from
+            my customer service background. I excel at collaborating with others
+            to solve complex challenges, and I find immense satisfaction in
+            overcoming difficult obstacles and driving collective success.
+          </p>
         </div>
         <div>
-          <Link href="/Contact">Contact</Link>
+          <h1>Experience</h1>
         </div>
+        <div>
+          <h1>Portfolio</h1>
+        </div>
+        <div>
+          <h1>Contact</h1>
+        </div>
+      </div>
+      <div class="footer">
+        <p>Footer</p>
       </div>
     </main>
   );
