a38b4397523fec2a9e988457aba9ffc9081e3cc9
Max Barnes <maxbarnes.dev@gmail.com>
2024-08-11 18:24:35 -0400

    header animation working

--- src/app/page.js
+++ src/app/page.js
@@ -1,49 +1,63 @@
+import Image from "next/image";
+
 export default function Landing() {
   return (
     <main>
       <div class="navbar">
-        <p class="logo">Max Barnes</p>
+        <p class="logo">
+          Max <span id="ln">Barnes</span>
+        </p>
         <ul class="nav-items">
           <li>About</li>
           <li>Education</li>
           <li>Experience</li>
           <li>Portfolio</li>
-          <li>Contact</li>
         </ul>
       </div>
 
+      <div class="section-header">
+        <div class="header-top">
+          <p>Take your </p>
+          <span id="fancy">
+            <p class="skill">Frontend</p>
+            <p class="skill">Backend</p>
+            <p class="skill">Database</p>
+            <p class="skill">Cybersecurity</p>
+            <p class="skill">Network</p>
+            <p class="skill">Cloud</p>
+          </span>
+        </div>
+        <p>to the Max</p>
+      </div>
       <div class="container">
         <div class="section-about">
           <h1>About</h1>
-          <p>
-            I'm Max Barnes, and I am a creator at heart. Whether it's
-            conceptualizing, creative writing, or figuring out how to make
-            day-to-day life easier; I am always driven by the idea of what could
-            be. I've cultivated leadership experience and customer service
-            skills throughout my career across various industries. However, my
-            journey into creating video games as a hobby sparked my fascination
-            with coding and its potential to take my ideas and build them into
-            anything.{" "}
-          </p>
+          <div class="about-container">
+            <p>
+              I'm Max Barnes, and I am a creator at heart. Whether it's
+              conceptualizing, creative writing, or figuring out how to make
+              day-to-day life easier; I am always driven by the idea of what
+              could be. I've cultivated leadership experience and customer
+              service skills throughout my career across various industries.
+              However, my journey into creating video games as a hobby sparked
+              my fascination with coding and its potential to take my ideas and
+              build them into anything.{" "}
+            </p>
+            <a>Download Resume</a>
+            <p>Last Updated: 8/11/2024</p>
+            <a href="https://www.linkedin.com/in/maxbarnesdev/">Linkedin</a>
+            <a href="https://github.com/Max-Barnes">Github</a>
+            <a href="mailto:maxbarnes.dev@gmail.com">Email</a>
+          </div>
         </div>
         <div>
           <h1>Education</h1>
-          <p>
-            I transitioned to the tech field because I recognized the growing
-            demand for the strong interpersonal soft skills I had developed from
-            my customer service background. I excel at collaborating with others
-            to solve complex challenges, and I find immense satisfaction in
-            overcoming difficult obstacles and driving collective success.
-          </p>
         </div>
         <div>
           <h1>Experience</h1>
         </div>
         <div>
           <h1>Portfolio</h1>
-        </div>
-        <div>
-          <h1>Contact</h1>
         </div>
       </div>
       <div class="footer">
